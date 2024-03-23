50289548343a23cbd54c97955ed8732244b9a98d
Александр Таловский <101811719+belmondoa@users.noreply.github.com>
2024-03-23 11:22:54 +0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'topbar.dart';
+import 'custom_navigation_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: ListView(
+       children: [
+          topbar(),
+          const SizedBox(height: 14),
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width, 
+            child: CustomTabBar()
+          ),
+       ],
+     ) 
+    );
+  }
+}
